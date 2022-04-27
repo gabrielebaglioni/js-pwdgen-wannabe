6282b3215efbbaee181d5aae4aab8fcddc069799
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-04-27 13:19:34 +0200

    inizializzato

--- .js
+++
@@ -1,5 +0,0 @@
-//chiedo nome e salvo
-// chiedo cognome e salvo
-//chiedo colore e salvo
-//genero un numero da affiancare e salvo 
-//accomuno tutto e genero password
---
+++ script.js
@@ -0,0 +1,11 @@
+//chiedo nome e salvo
+const userName = prompt ("inserisci il tuo nome")
+console.log(userName)
+// chiedo cognome e salvo
+const userLastName = prompt ("inserisci il tuo cognome")
+console.log(userLastName)
+//chiedo colore e salvo
+const userColor = prompt ("inserisci il tuo colore preferito")
+console.log(userColor)
+//genero un numero da affiancare e salvo 
+//accomuno tutto e genero password
